93c8514eeffe56915daf125354bd8cb1c21d70d1
Abhishek <abhisheku@capitalnumbers.net>
2025-02-28 11:07:42 +0530

    Add project files.

---
+++ CQRS/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CQRS.Models;
+
+namespace CQRS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Sample Product 1", Price = 9.99m },
+                new Product { Id = 2, Name = "Sample Product 2", Price = 19.99m },
+                new Product { Id = 3, Name = "Sample Product 3", Price = 29.99m }
+            );
+        }
+    }
+}
